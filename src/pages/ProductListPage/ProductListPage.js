692c0232ff307c6c0fe88457591870f65b74cabd
Tạ Phi Bằng <taphibang@gmail.com>
2020-06-03 11:10:43 +0700

    update code

--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import ProductList from '../../components/ProductList/ProductList';
 import ProductItem from '../../components/ProductItem/ProductItem';
 import {connect} from 'react-redux';
+import callApi from '../../utils/apiCaller';
+import { Link } from 'react-router-dom';
 
 class ProductListPage extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            products : []
+        };
+    }
+
+    componentDidMount(){
+        callApi('products','GET',null).then(res => {
+            this.setState({
+                products : res.data
+            });
+        });
+    }
+
+
     render() {
-        var {products} = this.props;
+        //var {products} = this.props;
+        var {products} = this.state;
+        
+        
         return (
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <button type="button" className="btn btn-info mb-10 mg-10">Thêm Sản Phẩm</button>
+                <Link to="/product/add" className="btn btn-info mb-10 mg-10">Thêm Sản Phẩm</Link>
                 <ProductList>
                     {this.showProducts(products)}
                 </ProductList>
